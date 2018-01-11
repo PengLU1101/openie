9209d7b04525204fb6baa7f0f07536c6ddce8fec
Peng Lu <lupeng@octal07.iro.umontreal.ca>
2018-01-11 10:23:25 -0500

    add new function

--- preprocess.py
+++ preprocess.py
@@ -13,6 +13,20 @@ import unicodedata
 import pprint
 from collections import defaultdict
 
+def unicodeToAscii(s):
+    return ''.join(
+        c for c in unicodedata.normalize('NFD', s)
+        if unicodedata.category(c) != 'Mn'
+    )
+
+def normalizeString(s):
+    s = unicodeToAscii(s.lower().strip())
+    s = re.sub(r"\"", r"'", s)
+    s = re.sub(r"/", r"", s)
+    s = re.sub(r"  ", r" ", s)
+    s = re.sub(r"[^a-zA-Z0-9.,'!?$[]\"<>-]+", r" ", s)
+    return s
+
 def load_data(path):
     with open(path, "r", encoding='iso-8859-15') as f:
         pairs = []
@@ -29,13 +43,33 @@ def load_data(path):
                 if string:
                     pair.append(string)
         return pairs
+
 def filter_long50(string):
     count = 0
     for i in string.split('\t'):
         count += len(i.split(" "))
     if count <= 50:
         return string
-
+def build_word_dict(sentences):
+    word_dict = defaultdict(int)
+    for s in sentences:
+        for fields in s:
+            word_dict[fields] += 1
+    return word_dict
+def build_label_dict(sentences):
+    label_dict = defaultdict(int)
+    for s in sentences:
+        for fields in s:
+            label_dict[fields] += 1
+    return label_dict
+def len_argsort(seq):
+    """
+    Function using to sort a list which its items are also list,the key to sort it is
+     the length of list in it.
+    Argument: seq(list of list, e.g. [[1, 2, 3], [4, 5], [c, d, e, g, f, k]])
+    Output: the sorted index(e. g [1, 0, 2 ])
+    """
+    return sorted(range(len(seq)), key=lambda x: len(seq[x]))
 
 
 def test():
